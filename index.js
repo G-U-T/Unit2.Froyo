2b2826b3b034ff48caf0d2e5815a3b9b3ba34a1b
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-10-31 14:17:20 -0400

    Input is saved as string, which is converted to into an array

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-prompt(`Type a comma-separated list of flavors. (Options are Vanilla, Coffee, and Strawberry.)`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`)
+const flavorString = String(prompt(`Type a comma-separated list of flavors. (Options are Vanilla, Coffee, and Strawberry.)`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`));
+const flavorArray = flavorString.split(`,`);
+console.log(flavorArray);
