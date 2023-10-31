c94eb0517f0df9d8f03de827acc3d3bf3e816b63
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-10-31 14:31:36 -0400

    Logs flavor counter object.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const flavorCounter = {
+	vanilla: 0,
+	coffee: 0,
+	strawberry: 0,
+};
+
+// Displays text in the middle of the HTML document.
+const updateTextLabel = (text) => {
+	const centerText = document.getElementById("center-text");
+	centerText.textContent = text;
+}
+
 const flavorString = String(prompt(`Type a comma-separated list of flavors. (Options are Vanilla, Coffee, and Strawberry.)`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`));
 const flavorArray = flavorString.split(`,`);
-console.log(flavorArray);
+
+for (flavorIndex in flavorArray) {
+	const flavor = flavorArray[flavorIndex];
+
+	// Checking the flavor names means it won't create a new key-value pair
+	// if something wasn't an available flavor.
+	if (flavor === `coffee` || flavor === `strawberry` || flavor === `vanilla`) {
+		flavorCounter[flavor] += 1;
+	}
+}
+
+console.log(flavorCounter);
+updateTextLabel(JSON.stringify(flavorCounter));
