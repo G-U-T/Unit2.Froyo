7e30e231aee63795dacb5adfd14335b09d4039ec
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-10-31 14:35:16 -0400

    Function returns the flavor counter object.

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const flavorCounter = {
-	vanilla: 0,
-	coffee: 0,
-	strawberry: 0,
-};
+const getFlavorCounter = (arrayOfFlavors) => {
+	const flavorCounter = {
+		vanilla: 0,
+		coffee: 0,
+		strawberry: 0,
+	};
+
+	for (flavorIndex in arrayOfFlavors) {
+		const flavor = arrayOfFlavors[flavorIndex];
+	
+		// Checking the flavor names means it won't create a new key-value pair
+		// if something wasn't an available flavor.
+		if (flavor === `coffee` || flavor === `strawberry` || flavor === `vanilla`) {
+			flavorCounter[flavor] += 1;
+		}
+	}
+
+	return flavorCounter;
+}
 
 // Displays text in the middle of the HTML document.
 const updateTextLabel = (text) => {
@@ -12,16 +26,7 @@ const updateTextLabel = (text) => {
 
 const flavorString = String(prompt(`Type a comma-separated list of flavors. (Options are Vanilla, Coffee, and Strawberry.)`, `vanilla,vanilla,vanilla,strawberry,coffee,coffee`));
 const flavorArray = flavorString.split(`,`);
-
-for (flavorIndex in flavorArray) {
-	const flavor = flavorArray[flavorIndex];
-
-	// Checking the flavor names means it won't create a new key-value pair
-	// if something wasn't an available flavor.
-	if (flavor === `coffee` || flavor === `strawberry` || flavor === `vanilla`) {
-		flavorCounter[flavor] += 1;
-	}
-}
+const flavorCounter = getFlavorCounter(flavorArray);
 
 console.log(flavorCounter);
 updateTextLabel(JSON.stringify(flavorCounter));
